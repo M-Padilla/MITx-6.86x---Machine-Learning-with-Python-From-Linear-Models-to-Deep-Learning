eb498cbfb6f71e1e4a325272a9c513718e487f0d
M-Padilla <M-Padilla@users.noreply.github.com>
2021-02-28 00:17:49 -0500

    Add files via upload

---
+++ Perceptron (No offset).py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 21 20:55:08 2021
+
+@author: user
+"""
+
+import numpy as np
+
+
+def perceptron_no_offset(x,y,epochs):
+    '''
+    x is a feature matrix
+    y is a label vector
+    Both need to be numpy arrays
+    '''
+    #Initialize array theta=0
+    theta = np.zeros(x.shape[1])
+    
+    for t in range(epochs):
+        for i in range(x.shape[0]):
+            #For every feature row vector in the feature matrix
+            if y[i] * np.dot(x[i], theta) <= 0:
+                '''
+                If y does not match the prediction of the linear classifier 
+                (product < 0) or the prediction is uncertain (product is 0), 
+                update the parameters of the theta vector.
+                '''
+                #Nudges the theta parameters in the same/opposite direction
+                #(determined by y) of the feature row vector. 
+                theta += y[i]*x[i]                           
+    return (theta)
+
+
+'''
+x = np.array([[-1,-1],
+                [1,0],
+                [-1,1.5]])
+
+y = np.array([1, -1, 1])
+'''
+
+print(perceptron(x,y,500))
+
+            
+  
---
+++ Perceptron (With offset).py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 21 23:54:35 2021
+
+@author: M-Padilla
+"""
+
+import numpy as np
+
+
+def perceptron(x,y,epochs):
+    '''
+    x is a feature matrix
+    y is a label vector
+    Both need to be numpy arrays
+    '''  
+    #Initialize array theta=0
+    theta = np.zeros(x.shape[1]+1) #theta[-1] is the offset, theta[:-1] are the parameters
+    
+    for t in range(epochs):
+        for i in range(x.shape[0]):
+            #For every feature row vector in the feature matrix            
+            if y[i] * (np.dot(x[i], theta[:-1]) + theta[2]) <= 0:
+                '''
+                If y does not match the prediction of the linear classifier 
+                (product < 0) or the prediction is uncertain (product is 0), 
+                update the parameters of the theta vector.
+                '''
+                #Nudges the theta parameters in the same/opposite direction
+                #(determined by the label y[i]) of the feature row vector.
+                theta[[0,1]] += y[i]*x[i]
+                #Increases/decreases the offset (determined by the label y[i]).
+                theta[2] += y[i]                                   
+    
+    return (theta)
+
+
+
+x = np.array([[-4, 2],
+                [-2,1],
+                [-1,-1],
+                [2,2],
+                [1,-2]])
+
+y = np.array([1, 1, -1, -1, -1])
+
+print(perceptron(x,y,2))
+
+            
+  
